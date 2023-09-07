2b04693df4b20c1b307d313fb13c47b143b54c88
GennadiyProg <gennadiys1906@gmail.com>
2023-09-07 19:31:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lab3/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.lab3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button mButton1 = findViewById(R.id.button);
+        TextView mTextView = findViewById(R.id.textView);
+        mTextView.setText((String)getIntent().getSerializableExtra("message"));
+        mButton1.setOnClickListener(this);
+    }
+    @SuppressLint("NonConstantResourceId")
+    public void onClick(View view) {
+        if (view.getId() == R.id.button) {
+            Intent intent = new Intent(getApplicationContext(), ActivityTwo.class);
+            startActivity(intent);
+        }
+    }
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+    }
+}
